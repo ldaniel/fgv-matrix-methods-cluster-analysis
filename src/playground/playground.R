890d015094cb31db7d481ef1099252c01abe8e1c
RodriGonca <rodrigo.goncalves@me.com>
2019-09-19 20:46:33 -0300

    playing with the data

--- src/playground/playground.R
+++ src/playground/playground.R
@@ -1,12 +1,47 @@
-# loading required libraries --------------------------------------------------
+# loading required libraries
 library(dplyr)
+library(readr)
+library(reshape2)
+library(markovchain)
+library(Matrix)
+library(matrixcalc)
 
-# load data
-
+# load data ----
 df <- readRDS('data/raw/skillset.rds')
+tm <- select(df, name, skill)
+# df <- df[1:50, ] # uncomment to reduce tha data amount for testing
+print(paste('Number of Unique Skills:    ', n_distinct(tm$skill)))
+print(paste('Number of Unique Employees: ', n_distinct(tm$name)))
 
-combinations <- t(combn(unique(df$skill), 2, simplify = TRUE)) %>% as_tibble
-combinations <- distinct(combinations, V1, V2)
+# get the cartesian product by name and create a transition matrix containing the number of unique employees with its skill combination. ----
+tm <- left_join(tm, tm, by = c('name'))
+tm <- acast(tm, skill.x ~ skill.y, fun.aggregate = n_distinct, value.var = 'name')
+tm[1:5, 1:5]
 
+# get the list of skill combinations ----
+skill_combinations <- t(combn(unique(df$skill), 2))
+skill_combinations[1:10, ]
 
+# check if transition matrix is symetric (expected for this usecase) and square ----
+print(paste('Is the Transition Matrix Symetric? ', isSymmetric(tm)))
+print(paste('Is the Transition Matrix Square?   ', is.square.matrix(tm)))
 
+# cating 1 to the transition matrix ----
+diag(tm) <- 1
+tm[1:5, 1:5]
+print(paste('Is the Transition Matrix Symetric? ', isSymmetric(tm)))
+print(paste('Is the Transition Matrix Square?   ', is.square.matrix(tm)))
+
+# calculating the row proportion ----
+tm = tm / colSums(tm)
+tm[1:5, 1:5]
+
+# creating the markov chain object ----
+mkv <- new('markovchain', transitionMatrix = tm, 
+           states = rownames(tm))
+
+# calculating the markov chain steady states ----
+steady <- t(steadyStates(mkv))
+
+# calculating the eigenvectors and eigenvalues for the transition matrix.
+eigen_v <- eigen(tm)
