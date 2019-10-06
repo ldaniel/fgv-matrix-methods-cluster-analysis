3f84baa6f213392f328e93adf287bcba16371bd0
ldaniel <contato@leandrodaniel.com>
2019-10-06 11:26:08 -0300

    Having fun in the playground

--- src/playground/playground_atividade_03_leandro.R
+++ src/playground/playground_atividade_03_leandro.R
@@ -41,26 +41,53 @@ ATIBAIA_drivers_num$ti = as.numeric(ATIBAIA_drivers_num$ti)
 ATIBAIA_drivers_num_z <- as.data.frame(lapply(ATIBAIA_drivers_num, scale))
 
 # calculating vars correlation
-cor(ATIBAIA_drivers_num_z)
+ggcorrplot(cor(ATIBAIA_drivers_num_z),
+           hc.order = TRUE,
+           lab = TRUE,
+           lab_size = 3,
+           method = "square",
+           colors = c("tomato2", "white", "springgreen3"),
+           title = "Correlation Matrix")
 
 # withinss abd betweenss according to its K ----------------------------------
 
 max_number_of_clusters <- nrow(ATIBAIA_drivers_num_z) - 1
 
-# initializing the vectors
-withinss_evolution  <- vector(mode = "numeric", length = max_number_of_clusters)
-betweenss_evolution <- vector(mode = "numeric", length = max_number_of_clusters)
+results <- tibble(k = numeric(), 
+                  withinss = numeric(), 
+                  betweenss = numeric(), 
+                  best_fit = numeric())
 
 for (count in seq(1, max_number_of_clusters, by = 1)) {
-  
   number_of_clusters <- as.numeric(count)
   KMeans_clustering <- kmeans(ATIBAIA_drivers_num_z, number_of_clusters, nstart = 20)
-  withinss_evolution[count] <- sum(KMeans_clustering$withinss)
-  betweenss_evolution[count] <- KMeans_clustering$betweenss
+
+  results <- add_row(results,
+                     k = number_of_clusters,
+                     withinss = sum(KMeans_clustering$withinss),
+                     betweenss = KMeans_clustering$betweenss,
+                     best_fit = abs(sum(KMeans_clustering$withinss) - KMeans_clustering$betweenss))
 }
 
-plot(withinss_evolution, type = "b")
-lines(t, w, type="b", col="red", lwd=2, pch=19)
+ggplot(data = results, aes(x = k)) +
+  geom_point(aes(y = withinss), color = 'blue') +
+  geom_point(aes(y = betweenss), color = 'red') +
+  geom_line(aes(y = withinss), color = 'blue') +
+  geom_line(aes(y = betweenss), color = 'red') +
+  geom_segment(aes(x = which.min(results$best_fit), 
+                   y = results$withinss[which.min(results$best_fit)], 
+                   xend = which.min(results$best_fit), 
+                   yend = results$betweenss[which.min(results$best_fit)]),
+               linetype = "dashed", color = "black") +
+  geom_label(aes(x = which.min(results$best_fit), 
+                 y = results$withinss[which.min(results$best_fit)], 
+                 label = paste('best k is ', which.min(results$best_fit))))
 
-plot(betweenss_evolution, type = "b")
-lines(t, w, type="b", col="red", lwd=2, pch=19)
+# choosing the K that best fits -----------------------------------------------
+KMeans_clustering <- kmeans(ATIBAIA_drivers_num_z, which.min(results$best_fit), nstart = 20)
+
+# Visualizing clusters
+plotcluster(ATIBAIA_drivers_num_z, KMeans_clustering$cluster)
+fviz_cluster(list(data = ATIBAIA_drivers_num_z, 
+                  cluster = KMeans_clustering$cluster),
+             show.clust.cent = T)
