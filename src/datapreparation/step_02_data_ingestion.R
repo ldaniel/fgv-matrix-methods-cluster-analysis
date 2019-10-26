596f67d092b626ae4a7e38027e100377a34f1ee3
ldaniel <contato@leandrodaniel.com>
2019-10-26 15:32:59 -0300

    Updatão

--- src/datapreparation/step_02_data_ingestion.R
+++ src/datapreparation/step_02_data_ingestion.R
@@ -7,16 +7,3 @@ target_data <- read_xlsx(paste(dataDirectory, "base_casamentos.xlsx", sep = ""),
                          sheet = "CASAMENTOS", 
                          skip = 1, 
                          col_names = TRUE)
-
-# renaming columns
-names(target_data)[1]  <- "id"
-names(target_data)[2]  <- "aceitar_convite"
-names(target_data)[3]  <- "gostar_doces"
-names(target_data)[4]  <- "emocionar"
-names(target_data)[5]  <- "conhecer_pessoas"
-names(target_data)[6]  <- "diversao_amigos"
-names(target_data)[7]  <- "gostar_fotos"
-names(target_data)[8]  <- "cansar_cerimonia"
-names(target_data)[9]  <- "casamento_civil"
-names(target_data)[10] <- "idade"
-names(target_data)[11] <- "sexo"
--- src/datapreparation/step_04_data_enhancement.R
+++
@@ -1,5 +0,0 @@
-## ---- step_04_data_enhancement.R
-
-# this step aims to improve the analysis by adding auxiliary information ------
-
-# to-do
